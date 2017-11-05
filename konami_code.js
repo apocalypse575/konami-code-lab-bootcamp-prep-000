5422eeae537a900d6b27920b390fea74d5c7559c
apocalypse575 <apocalypse575@gmail.com>
2017-11-05 20:22:22 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,14 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+function init(e) {
+  const input = document.querySelector('body')
 
+  input.addEventListener('keydown', function(e) {
+  if (e.which === code[alphabet]) {
+    return alert("Hurray!")
+  }
+  else {
+    index = 0;
+  }
+})
 }
